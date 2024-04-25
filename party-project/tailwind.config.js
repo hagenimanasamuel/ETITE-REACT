3054423ca9f33799b05f08807bf3ab20584b43ac
HAGENIMANA Samuel <smlhagenimana@gmail.com>
2024-04-25 11:51:19 +0200

    Added tailwindcss in the react project

---
+++ party-project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/Party-Project/Admin/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
