234d56921ce9dd92876513b453ea4869c3a287d7
HAGENIMANA Samuel <smlhagenimana@gmail.com>
2024-04-26 12:35:16 +0300

    Still editing

--- party-project/src/components/Party-Project/Admin/AdminContent.jsx
+++ party-project/src/components/Party-Project/Admin/AdminContent.jsx
@@ -4,11 +4,10 @@ export default function AdminContent() {
     return(
         <>
             <div className="admin-content">
-                <div className="content">Hello This is thr component that contains all home admin contents</div>
-                <div className="content">Hello This is thr component that contains all home admin contents</div>
-                <div className="content">Hello This is thr component that contains all home admin contents</div>
-                <div className="content">Hello This is thr component that contains all home admin contents</div>
-                <div className="content">Hello This is thr component that contains all home admin contents</div>
+                <div className="content text-2xl font-bold">Hello This is the component that contains all home admin contents</div>
+                <div className="content text-2xl font-bold">Hello This is the component that contains all home admin contents</div>
+                <div className="content text-2xl font-bold">Hello This is the component that contains all home admin contents</div>
+                <div className="content text-2xl font-bold">Hello This is the component that contains all home admin contents</div>
             </div>
         </>
     )
